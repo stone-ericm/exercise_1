1bfc25bc3a0d26a692d495a3776c60c80cefeb81
Eric Stone <stone11375@gmail.com>
2019-01-23 19:52:04 +0000

    Completed iteration_with_lists exercise

--- exercises/iteration_with_lists/iteration.py
+++ exercises/iteration_with_lists/iteration.py
@@ -1,12 +1,33 @@
+import sys
+
 def my_loop(x):
-    # write your code here
-    # you'll have to delete "pass"
-    pass
+    i = 1
+    while i < x:
+        print i
+        i += 1
 
 
-def my_reverse_loop(y):
-    # write your code here
-    # you'll have to delete "pass"
-    pass
+def my_reverse_loop(x):
+    i = x-1
+    while i > 0:
+        print i
+        i -= 1
 
-print (my_loop(10))
+def main():
+    if __name__ == "__main__":
+        try:
+            sys.argv[2];
+        except IndexError:
+            print("Please include the function to be run followed by the number to use.")
+            exit()
+        function = str(sys.argv[1])
+        number = int(sys.argv[2])
+        if function == "loop":
+            my_loop(number)
+        elif function == "rloop":
+            my_reverse_loop(number)
+
+            
+main()
+# print (my_loop(10))
+
